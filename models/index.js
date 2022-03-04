ce22f158247abd86057a904a6cb5bc16f6bcecb2
TirapatS <tirapat.s02@gmail.com>
2022-03-04 10:20:29 +0100

    Removed an oopsie

--- models/index.js
+++ models/index.js
@@ -3,11 +3,11 @@ const knex = require('knex')({
 	debug: true,
 	client: 'mysql',
 	connection: {
-		host: process.env.DB_HOST || 'localhost',
-		port: process.env.DB_PORT || 8889,
-		database: process.env.DB_NAME || 'photos',
-		user: process.env.DB_USER || 'root',
-		password: process.env.DB_PASSWORD || '',
+		host: process.env.DB_HOST,
+		port: process.env.DB_PORT,
+		database: process.env.DB_NAME,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD
 	}
 });
 
