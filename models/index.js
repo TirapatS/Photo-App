170016e767690cd82e8084c752586b96b318c719
TirapatS <tirapat.s02@gmail.com>
2022-03-06 14:49:57 +0100

    connection port, user DB_PORT or another

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ const knex = require('knex')({
 	client: 'mysql',
 	connection: {
 		host: process.env.DB_HOST,
-		port: process.env.DB_PORT,
+		port: process.env.DB_PORT || 3306,
 		database: process.env.DB_NAME,
 		user: process.env.DB_USER,
 		password: process.env.DB_PASSWORD
