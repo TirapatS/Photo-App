52e34de571a9f145e8bcd4a110f1615cc230e234
TirapatS <tirapat.s02@gmail.com>
2022-04-01 08:01:09 +0200

    Changed enpoints, instead of /user/register now /register

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,20 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middlewares/auth');
+const userValidationRules = require('../validation/user');
+const userController = require('../controllers/user_controller');
+
 
 /* GET / */
 router.get('/', (req, res, next) => {
 	res.send({ status: 200, message: 'Oh hello there, the connection was successful!' });
 });
 
-//router.use('/albums', require('./album'));
-
 router.use('/photos', auth.basic, require('./photos'));
 router.use('/albums', auth.basic, require('./albums'));
 router.use('/user', require('./users'));
+
+/* Register new users */
+router.post('/register', userValidationRules.createRules, userController.register);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user_controller');
-const userValidationRules = require('../validation/user');
-const photoValidationRules = require('../validation/photo');
 const auth = require('../middlewares/auth')
-
-
 
 /* Get user info */
 router.get('/', auth.basic, userController.getUser);
-
-
-/* Register new users */
-router.post('/register', userValidationRules.createRules, userController.register);
-
 
 module.exports = router;
